12bd7985b5047ed73e1a6e2642376d43a63f88c0
Yuriy Chaban <Yuriy Chaban>
2016-12-12 21:07:00 +0200

    add html for styling home page

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// DESCTOP MENU
+$('#toggle').click(function() {
+	$(this).toggleClass('active');
+	$('#overlay').toggleClass('open');
+	$('#toggle').parents('.inside-page-header').css('padding: 0;')
+});
+
+$(document).ready(function() {
+	$('.special-offers-slider').slick({
+		// autoplay: true,
+		// autoplaySpeed: 3000,
+		infinite: true,
+		// speed: 1000,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerPadding: '20px',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				centerMode: true,
+				centerPadding: '20px',
+				slidesToShow: 1
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				arrows: true,
+				centerMode: true,
+				centerPadding: '0px',
+				slidesToShow: 1
+			}
+		}
+		]
+	});
+});
+//scroll to top
+jQuery(document).ready(function($){
+    // browser window scroll (in pixels) after which the "back to top" link is shown
+    var offset = 300,
+        //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+        offset_opacity = 1200,
+        //duration of the top scrolling animation (in ms)
+        scroll_top_duration = 700,
+        //grab the "back to top" link
+        $back_to_top = $('.cd-top');
+
+    //hide or show the "back to top" link
+    $(window).scroll(function(){
+    	( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+    	if( $(this).scrollTop() > offset_opacity ) {
+    		$back_to_top.addClass('cd-fade-out');
+    	}
+    });
+
+    //smooth scroll to top
+    $back_to_top.on('click', function(event){
+    	event.preventDefault();
+    	$('body,html').animate({
+    		scrollTop: 0 ,
+    	}, scroll_top_duration
+    	);
+    });
+
+  });
